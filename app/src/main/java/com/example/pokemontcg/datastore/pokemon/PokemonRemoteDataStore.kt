c70cf27bfdee42a779525bd0544ddbc707638b44
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-11 20:25:31 +0700

    Create PokemonRemoteDataStore.kt

---
+++ app/src/main/java/com/example/pokemontcg/datastore/pokemon/PokemonRemoteDataStore.kt
@@ -0,0 +1,19 @@
+package com.example.pokemontcg.datastore.pokemon
+
+import com.example.pokemontcg.model.PokemonCard
+import com.example.pokemontcg.webservice.PokemonTcgService
+
+class PokemonRemoteDataStore(private val pokemonTcgService: PokemonTcgService) :
+    PokemonCardDataStore {
+
+    override suspend fun getPokemons(set: String): MutableList<PokemonCard>? {
+        val response = pokemonTcgService.getCards(set)
+        if (response.isSuccessful) return response.body()?.cards
+
+        throw Exception("A problem happened when request data, status error ${response.code()}")
+    }
+
+    override suspend fun addAll(set: String, pokemons: MutableList<PokemonCard>?) {
+
+    }
+}
