38f2bf2b157649a7059b3a0465d75d146ae9953c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-11 19:49:32 +0700

    Create PokemonCardDataStore.kt

---
+++ app/src/main/java/com/example/pokemontcg/datastore/pokemon/PokemonCardDataStore.kt
@@ -0,0 +1,8 @@
+package com.example.pokemontcg.datastore.pokemon
+
+import com.example.pokemontcg.model.PokemonCard
+
+interface PokemonCardDataStore {
+    suspend fun getPokemons(set: String): MutableList<PokemonCard>?
+    suspend fun addAll(set: String, pokemons: MutableList<PokemonCard>?)
+}
