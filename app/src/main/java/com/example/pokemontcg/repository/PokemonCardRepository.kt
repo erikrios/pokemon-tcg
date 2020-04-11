614ff69d8b86df4c5e0542dbf9a404fa5c12c137
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-11 19:59:00 +0700

    Create PokemonCardRepository.kt

---
+++ app/src/main/java/com/example/pokemontcg/repository/PokemonCardRepository.kt
@@ -0,0 +1,30 @@
+package com.example.pokemontcg.repository
+
+import com.example.pokemontcg.datastore.pokemon.PokemonCardDataStore
+import com.example.pokemontcg.datastore.set.PokemonSetRemoteDataStore
+import com.example.pokemontcg.model.PokemonCard
+
+class PokemonCardRepository private constructor() {
+    private var pokemonCardLocalDataStore: PokemonCardDataStore? = null
+    private var pokemonCardRemoteDataStore: PokemonCardDataStore? = null
+
+    fun init(
+        pokemonCardLocalDataStore: PokemonCardDataStore,
+        pokemonCardRemoteDataStore: PokemonCardDataStore
+    ) {
+        this.pokemonCardLocalDataStore = pokemonCardLocalDataStore
+        this.pokemonCardRemoteDataStore = pokemonCardRemoteDataStore
+    }
+
+    suspend fun getPokemons(set: String): MutableList<PokemonCard>? {
+        val cache = pokemonCardLocalDataStore?.getPokemons(set)
+        if (cache != null) return cache
+        val response = pokemonCardRemoteDataStore?.getPokemons(set)
+        pokemonCardLocalDataStore?.addAll(set, response)
+        return response
+    }
+
+    companion object {
+        val instance by lazy { PokemonCardRepository() }
+    }
+}
