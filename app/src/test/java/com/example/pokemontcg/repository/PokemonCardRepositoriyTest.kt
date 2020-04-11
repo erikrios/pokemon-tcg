edf132d63318e1ed995497486864ab8b4317e07d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-11 20:40:22 +0700

    Create PokemonCardRepositoriyTest.kt for unit testing

---
+++ app/src/test/java/com/example/pokemontcg/repository/PokemonCardRepositoriyTest.kt
@@ -0,0 +1,65 @@
+package com.example.pokemontcg.repository
+
+import com.example.pokemontcg.datastore.pokemon.PokemonCardDataStore
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
+class PokemonCardRepositoriyTest {
+    @Mock
+    var localCardDataStore: PokemonCardDataStore? = null
+
+    @Mock
+    var remoteCardDataStore: PokemonCardDataStore? = null
+
+    var pokemonCardRepository: PokemonCardRepository? = null
+
+    @Before
+    fun init() {
+        MockitoAnnotations.initMocks(this)
+        pokemonCardRepository = PokemonCardRepository.instance.apply {
+            init(localCardDataStore!!, remoteCardDataStore!!)
+        }
+    }
+
+    @Test
+    fun shouldNotGetPokemonsFromRemoteWhenLocalIsNotNull() {
+        runBlocking {
+            `when`(localCardDataStore?.getPokemons(anyString())).thenReturn(mutableListOf())
+            pokemonCardRepository?.getPokemons(anyString())
+
+            verify(remoteCardDataStore, never())?.getPokemons(anyString())
+            verify(localCardDataStore, never())?.addAll(anyString(), any())
+        }
+    }
+
+    @Test
+    fun shouldCallGetPokemonsFromRemoteAndSaveToLocalWhenLocalIsNull() {
+        runBlocking {
+            `when`(localCardDataStore?.getPokemons(anyString())).thenReturn(null)
+            `when`(remoteCardDataStore?.getPokemons(anyString())).thenReturn(any())
+            pokemonCardRepository?.getPokemons("Test set")
+
+            verify(remoteCardDataStore, times(1))?.getPokemons(anyString())
+            verify(localCardDataStore, times(1))?.addAll(anyString(), any())
+        }
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenRemoteThrowAnException() {
+        runBlocking {
+            `when`(localCardDataStore?.getPokemons(anyString())).thenReturn(null)
+            `when`(remoteCardDataStore?.getPokemons(anyString())).thenAnswer { throw  Exception() }
+
+            try {
+                pokemonCardRepository?.getPokemons("Tes set")
+            } catch (ex: Exception) {
+
+            }
+        }
+    }
+}
