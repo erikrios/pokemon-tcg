d43367dcbb8d15c4c65c8563aa7b142b60d21caa
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-12 19:53:47 +0700

    Create PokemonCardDetailFragment.kt

--- app/src/main/java/com/example/pokemontcg/detail/PokemonCardDetailFragment.kt
+++ app/src/main/java/com/example/pokemontcg/detail/PokemonCardDetailFragment.kt
@@ -1,17 +1,24 @@
 package com.example.pokemontcg.detail
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.pokemontcg.R
+import com.example.pokemontcg.model.PokemonCard
+import kotlinx.android.synthetic.main.fragment_pokemon_card_detail.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class PokemonCardDetailFragment : Fragment() {
+
+    private lateinit var viewModel: PokemonCardDetailViewModel
+
+    private val args: PokemonCardDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +28,31 @@ class PokemonCardDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_pokemon_card_detail, container, false)
     }
 
+    @SuppressLint("FragmentLiveDataObserve")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pokemonCard = args.pokemonCard
+        viewModel = ViewModelProvider(this).get(PokemonCardDetailViewModel::class.java).apply {
+            viewState.observe(
+                this@PokemonCardDetailFragment,
+                Observer(this@PokemonCardDetailFragment::handleState)
+            )
+            pokemonCard?.let { setData(it) }
+        }
+    }
+
+    private fun handleState(viewState: PokemonCardDetailViewState) {
+        viewState.data?.let { showDetail(it) }
+    }
+
+    private fun showDetail(data: PokemonCard) {
+        Glide.with(this)
+            .load(data.image)
+            .into(ivLogo)
+
+        tvName.text = data.name
+        tvRarity.text = data.rarity
+        tvSeries.text = data.series
+    }
 }
