5056a5e50900f1fc2d6eddfcb24aea57d48153a6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-12 20:01:46 +0700

    Add navController

--- app/src/main/java/com/example/pokemontcg/main/MainActivity.kt
+++ app/src/main/java/com/example/pokemontcg/main/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.pokemontcg.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.pokemontcg.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(tbMain)
+        navController = navHost.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
